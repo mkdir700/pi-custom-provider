71d33a6508a1d335bc4e8a5314f91cb32c451774
mkdir700 <mkdir700@gmail.com>
2026-02-11 11:35:23 +0800

    Initial commit

---
+++ index.ts
@@ -0,0 +1,47 @@
+import type { ExtensionAPI } from "@mariozechner/pi-coding-agent";
+
+export default function (pi: ExtensionAPI) {
+  pi.registerProvider("codex-api", {
+    baseUrl: process.env.OPENAI_CODEX_BASE_URL,
+    apiKey: process.env.OPENAI_CODEX_API_KEY,
+    api: "openai-responses",
+    models: [
+      {
+        id: "gpt-5.3-codex",
+        name: "GPT-5.3-Codex",
+        reasoning: true,
+        input: ["text", "image"],
+        cost: { input: 0, output: 0, cacheRead: 0, cacheWrite: 0 },
+        contextWindow: 200000,
+        maxTokens: 8192,
+      },
+      {
+        id: "gpt-5.2",
+        name: "GPT-5.2",
+        reasoning: true,
+        input: ["text", "image"],
+        cost: { input: 0, output: 0, cacheRead: 0, cacheWrite: 0 },
+        contextWindow: 200000,
+        maxTokens: 8192,
+      },
+      {
+        id: "gpt-5.2-codex",
+        name: "GPT-5.2-Codex",
+        reasoning: true,
+        input: ["text", "image"],
+        cost: { input: 0, output: 0, cacheRead: 0, cacheWrite: 0 },
+        contextWindow: 200000,
+        maxTokens: 8192,
+      },
+      {
+        id: "gpt-5.1-codex-max",
+        name: "GPT-5.1-Codex-Max",
+        reasoning: true,
+        input: ["text", "image"],
+        cost: { input: 0, output: 0, cacheRead: 0, cacheWrite: 0 },
+        contextWindow: 200000,
+        maxTokens: 8192,
+      },
+    ],
+  });
+}
